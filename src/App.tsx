f128442feeb657f86a39db1d366f209c76830c7b
marcos-miotto <marcos.h.miotto@gmail.com>
2024-09-19 21:37:08 -0300

    ajuste imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, useLocation, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
 import Sobre from './pages/sobre';

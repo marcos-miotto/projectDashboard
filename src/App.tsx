516a7678405755f12d5eac7396e83a3b5a089921
marcos-miotto <marcos.h.miotto@gmail.com>
2024-09-19 21:32:49 -0300

    navBar / useLocation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
 import Sobre from './pages/sobre';
@@ -7,15 +7,24 @@ import Dashboard from './pages/dashboard';
 import Login from './pages/login';
 
 const App = () => {
+  const location = useLocation();
   return (
     <Router>
       <div className="app">
         <nav>
           <ul>
-            <li><a href="/login">Login</a></li>
-            <li><a href="/home">Home</a></li>
-            <li><a href="/sobre">Sobre</a></li>
-            <li><a href="/dashboard">Dashboard</a></li>
+            <li>
+              <Link to="/login" className={location.pathname === '/login' ? 'active' : ''}>Login</Link>
+            </li>
+            <li>
+              <Link to="/home" className={location.pathname === '/home' ? 'active' : ''}>Home</Link>
+            </li>
+            <li>
+              <Link to="/dashboard" className={location.pathname === '/dashboard' ? 'active' : ''}>Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/sobre" className={location.pathname === '/sobre' ? 'active' : ''}>Sobre</Link>
+            </li>
           </ul>
         </nav>
         <Routes>
